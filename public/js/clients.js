09cefcf3c3f34fd0802ca5731763e83916d134a9
Mark McSpadden <markmcspadden@gmail.com>
2013-09-25 00:51:35 -0500

    Use handlebars for both client sections

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,11 +1,23 @@
 function loadClients() {
-  
+  loadCurrentClients();
+  loadPotentialClients();
+}
+
+function loadCurrentClients() {
+  var html = clientsHTML(clients["clients"]["current"], "info");
+  $("#current_clients").append(html);
+}
+
+function loadPotentialClients() {
+  var html = clientsHTML(clients["clients"]["potential"], "");
+  $("#potential_clients").append(html);
+}
+
+function clientsHTML(clients, status) {
   var source   = $("#clients-template").html();
   var template = Handlebars.compile(source);
-  var context = {clients:clients["clients"]["current"]}
+  var context = {clients:clients, status:status}
   var html    = template(context);
 
-
-  $("#current_clients").append(html);
-  
+  return html;
 }
